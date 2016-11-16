424e15e19f2ea13ee96923f067d4eb8deeba8be2
Arnim Eijkhoudt <uforia@dhcp.net>
2016-11-16 23:53:30 +0100

    Add files via upload

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# Generic Timestamp Converter for Windows
+## Generic Timestamp Converter for Windows
 
 Windows is a mess of timestamp conventions, and we were getting pretty annoyed with having to
 mess around with a plethora of tooling to convert them into something resembling a human-
@@ -15,12 +15,19 @@ into something human readable. You can run it interactively, directly from the c
 you can pipe / redirect a hex string directly to it. Inter-byte spacing is not important, the
 script will automatically compress it into something it will understand.
 
+# Comments
+
+There is some very basic checking done to see if the timestamp appears to be correct.
+VAX/VMS timestamps might be working, but there's no way to really check. Feedback welcome!
+
+## Usage
+
 # Interactive mode
 
 ... $ ./wintimestamp.py  
 Gimme a timestamp: e0 07 0a 00 06 00 0f 00  11 00 2c 00 31 00 7f 00  
 Registry timestamp: 2016-10-15 17:44:49.127  
-... $  
+... $
 
 # Command-line mode
 
@@ -32,4 +39,5 @@ FAT timestamp: 2008-11-05 19:52:34
 
 ... $ echo "C2 3F 17 B7 2E C9 C7 01" | ./wintimestamp.py  
 NTFS timestamp: 2007-07-18 11:28:08.078124  
-... $  
+VAX/VMS timestamp: 2265-06-02 11:28:08.078124  
+... $
--- wintimestamp.py
+++ wintimestamp.py
@@ -1,59 +1,31 @@
 #!/usr/bin/env python
 
-# Generic Timestamp Converter for Windows
-# ---------------------------------------
-#
-# Version 0.8 (c) Arnim Eijkhoudt, KPN-CERT
-#
-# Windows is a mess of timestamp conventions, and we were getting pretty annoyed with having to
-# mess around with a plethora of tooling to convert them into something resembling a human-
-# readable format. Hence, this poorly cobbled-together script was born. It should work fine under
-# most versions of Python (tested with 2.7/3.x).
-#
-# If you like this, good for you!
-# If you don't like this, I don't wanna hear about it!
-#
-# What it does
-# ------------
-#
-# This script is capable of detecting and converting Windows' Registry-, NTFS- and FAT-timestamps
-# into something human readable. You can run it interactively, directly from the command-line or
-# you can pipe / redirect a hex string directly to it. Inter-byte spacing is not important, the
-# script will automatically compress it into something it will understand.
-#
-# Interactive mode
-# ----------------
-# ... $ ./timestamp.py
-# Gimme a timestamp: e0 07 0a 00 06 00 0f 00  11 00 2c 00 31 00 7f 00
-# Registry timestamp: 2016-06-15 17:44:49.127
-# ... $
-#
-# Command-line mode
-# -----------------
-# ... $ ./timestamp.py 919e6539
-# FAT timestamp: 2008-11-05 19:52:34
-# ... $
-#
-# Pipe/redirect mode
-# ------------------
-# ... $ echo "01cb17701e9c885a" | ./timestamp.py
-# NTFS timestamp: 2010-06-29 09:47:42.754212
-# ... $
+# Version 0.9 (c) Arnim Eijkhoudt, KPN-CERT
 
 import sys,datetime
+
 if len(sys.argv)<2:
 	if not sys.stdin.isatty():
 		for line in sys.stdin:
 			dt=line.replace(' ','').strip()
 			break
 	else:
-		dt=''.join(raw_input("Gimme a timestamp: ")).replace(' ','')
+		dt=''.join(input("Gimme a timestamp: ")).replace(' ','')
 else:
 	dt=''.join(sys.argv[1:]).replace(' ','')
 if len(dt)==16:
-	# ZOMG it's NTFS!
-	us=int("".join(reversed([dt[i:i+2] for i in range(0, len(dt), 2)])),16)/10.
-	print("NTFS timestamp: "+str(datetime.datetime(1601,1,1)+datetime.timedelta(microseconds=us)))
+	# ZOMG it's NTFS! Or is it VAX/VMS... *shudder*
+	swapped="".join(reversed([dt[i:i+2] for i in range(0, len(dt), 2)]))
+	try:
+		ntfs=datetime.datetime(1601,1,1)+datetime.timedelta(microseconds=int(swapped,16)/10.)
+		vax=datetime.datetime(1858,11,17)+datetime.timedelta(microseconds=int(swapped,16)/10.)
+	except OverflowError:
+		print("Timestamp is out of range!")
+	else:
+		if int(str(ntfs)[0:4])>2006:
+			# Yeah this is probably NTFS, since Windows Vista wasn't born before 2006
+			print("NTFS timestamp: "+str(ntfs))
+		print("VAX/VMS timestamp: "+str(vax))
 if len(dt)==32:
 	# Crikey, a registry timestamp
 	year=dt[2:4]+dt[0:2]
